7dabcdf6ea87d07ee98da8c6922ea13d668f2335
Common Training user <CT0t1868@TechMahindra.com>
2019-05-29 15:44:03 +0530

    Spring Bootification Start

--- src/main/java/org/superbiz/moviefun/ActionServlet.java
+++ src/main/java/org/superbiz/moviefun/ActionServlet.java
@@ -16,9 +16,11 @@
  */
 package org.superbiz.moviefun;
 
-import org.apache.commons.lang.StringUtils;
 
-import javax.ejb.EJB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,13 +31,14 @@ import java.util.List;
 /**
  * @version $Revision$ $Date$
  */
+@Component
 public class ActionServlet extends HttpServlet {
 
     private static final long serialVersionUID = -5832176047021911038L;
 
     public static int PAGE_SIZE = 5;
 
-    @EJB
+    @Autowired
     private MoviesBean moviesBean;
 
     @Override
