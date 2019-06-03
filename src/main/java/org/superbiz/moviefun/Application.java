6349619c3503a9c45a9d7bf000f64a267ed5c70d
Common Training user <CT0t1868@TechMahindra.com>
2019-06-03 18:27:19 +0530

    replatformingBackgroundJobsWithAmqp added

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -31,20 +31,17 @@ public class Application {
 
     @Bean
     public BlobStore blobStore(
-        ServiceCredentials serviceCredentials,
-        @Value("${vcap.services.photo-storage.credentials.endpoint:#{null}}") String endpoint
+            ServiceCredentials serviceCredentials,
+            @Value("${vcap.services.photo-storage.credentials.endpoint:#{null}}") String endpoint
     ) {
         String photoStorageAccessKeyId = serviceCredentials.getCredential("photo-storage", "user-provided", "access_key_id");
         String photoStorageSecretKey = serviceCredentials.getCredential("photo-storage", "user-provided", "secret_access_key");
         String photoStorageBucket = serviceCredentials.getCredential("photo-storage", "user-provided", "bucket");
-
         AWSCredentials credentials = new BasicAWSCredentials(photoStorageAccessKeyId, photoStorageSecretKey);
         AmazonS3Client s3Client = new AmazonS3Client(credentials);
-
         if (endpoint != null) {
             s3Client.setEndpoint(endpoint);
         }
-
         return new S3Store(s3Client, photoStorageBucket);
     }
 }
